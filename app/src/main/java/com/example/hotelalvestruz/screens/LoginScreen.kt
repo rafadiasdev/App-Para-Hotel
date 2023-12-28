010b14ef357b83480171897f30a2aa25f195cc25
Rafael <comercialrafaelalves@gmail.com>
2023-12-28 18:27:04 -0400

    Hotel Alvestruz

--- app/src/main/java/com/example/hotelalvestruz/screens/LoginScreen.kt
+++ app/src/main/java/com/example/hotelalvestruz/screens/LoginScreen.kt
@@ -3,36 +3,46 @@ package com.example.hotelalvestruz.screens
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.hotelalvestruz.ui.theme.HotelAlvestruzTheme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.hotelalvestruz.R
 
 @Composable
 fun LoginScreen() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color.Black),
+            .background(color = Color.Blue),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Text(
-            text = "Seja Bem-Vindo",
-            modifier = Modifier,
-            color = Color.White
+            text = stringResource(R.string.app_name),
+            color = Color.White,
+            fontSize = 30.sp,
+            fontFamily = FontFamily.SansSerif,
+            fontWeight = FontWeight.Bold
         )
+        Spacer(modifier = Modifier.padding(20.dp))
     }
 }
 
-@Preview(showSystemUi = true, showBackground = true)
+
+
+@Preview(showSystemUi = true)
 @Composable
-fun LoginScreenPreview() {
-    HotelAlvestruzTheme {
+private fun LoginScreenPreview() {
         LoginScreen()
-    }
 }
