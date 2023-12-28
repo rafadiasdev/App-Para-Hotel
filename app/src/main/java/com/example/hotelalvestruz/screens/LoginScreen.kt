1e61eec780f3b7ea0cf89a74c294475945db1baa
Rafael <comercialrafaelalves@gmail.com>
2023-12-27 23:29:42 -0400

    Hotel Alvestruz

--- app/src/main/java/com/example/hotelalvestruz/screens/LoginScreen.kt
+++ app/src/main/java/com/example/hotelalvestruz/screens/LoginScreen.kt
@@ -1,23 +1,30 @@
 package com.example.hotelalvestruz.screens
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.hotelalvestruz.ui.theme.HotelAlvestruzTheme
 
 @Composable
 fun LoginScreen() {
     Column(
-        modifier = Modifier,
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.Black),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
         Text(
-            text = "Hello Word"
+            text = "Seja Bem-Vindo",
+            modifier = Modifier,
+            color = Color.White
         )
     }
 }
